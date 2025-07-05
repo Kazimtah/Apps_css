6c4710f65a6d1b2f72f61e61feacb54116821a27
Kazimtha <kazimhamid02@gmail.com>
2025-07-05 17:51:10 +0200

    Adding a while loop to our studying

---
+++ javascript/whileloop.js
@@ -0,0 +1,58 @@
+//  let x = 5;
+//  if (x%2==0){
+//     console.log('Even');
+//  }else {
+//     console.log('Odd');
+//  }
+
+//  let y=2, z=3, t=4, h=0;
+//  y-=2;
+//  h += y*z -t**2;
+//  console.log(h);
+
+
+// if (x<0){
+//     console.log("Invalid");
+// } else if (x < 10 ){
+//     console.log('Fail');
+// } else if (x<12){
+//     console.log('Conditional');
+// } else if(x<=20){
+//     console.log('Pass');
+// }else {
+//     console.log('Invalid Input');
+// }
+
+// x = 22;
+// if (20<x<0){
+//     console.log(`${x} You entered the invalid number which is`);
+// } else if (x<10){
+//     console.log(`${x} is Failed`);
+// }else if(x<12) {
+//     console.log(`${x} is Conditional`);
+// } else if(x<=20) {
+//     console.log('Passed')
+// }
+
+// let x = 2;
+// console.log(x--);
+// console.log(x)
+
+// let x =2;
+// let y = 3;
+// let z =1;
+
+// console.log(++x-y*((z++)+2)-(z+x));
+
+// let x =0;
+// while (x<100){
+//     console.log('Hello');
+//     x++;
+// }
+
+let x =1 ;
+while (x<10){
+
+    console.log(x);
+    x++;
+}
