ecde272892285fb13c05510c456d3d16495f13fa
Kazimtha <kazimhamid02@gmail.com>
2025-07-13 12:35:15 +0200

    Adding a login and Menu

---
+++ animation/lastjavascript.js
@@ -0,0 +1,46 @@
+const promoMessage = 12;
+const reminderMessage= 15;
+const welcomeMEssage =8;
+const supportMessage = 5;
+
+// Total number of message sent out
+
+const totalMessage = promoMessage + reminderMessage + welcomeMEssage+ supportMessage;
+console.log(`The total number of Message send it ${totalMessage}`);
+const averageMessage = (promoMessage + reminderMessage + welcomeMEssage+ supportMessage)/4;
+console.log(`The average Message send out is ${averageMessage}`);
+
+let bootdevCourseRating = 4;
+bootdevCourseRating++;
+console.log(bootdevCourseRating);
+bootdevCourseRating +=5;
+console.log(bootdevCourseRating)
+bootdevCourseRating--;
+console.log(bootdevCourseRating);
+bootdevCourseRating -= 2;
+console.log(bootdevCourseRating);
+
+let numFailedMessage = 1336;
+numFailedMessage++;
+console.log(numFailedMessage);
+
+let sendMessage;
+let delieveredMessage;
+let failedMessage;
+
+console.log("Sent is", sendMessage);
+console.log("Delivered is", delieveredMessage);
+console.log("Faild message is ", failedMessage);
+
+let answerTolife = 42;
+let answerToTheUniverse = "42";
+
+console.log(typeof answerToTheUniverse , typeof answerTolife);
+console.log(typeof answerTolife);
+const answereToAll = answerToTheUniverse + answerTolife;
+console.log(answereToAll);
+
+const averageOpenRate =0.23, displayMessage = "Is the average ope rate of your messages";
+
+console.log("Averate openn Rate", averageMessage);
+console.log("Display message is ", displayMessage);
